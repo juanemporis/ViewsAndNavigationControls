2245ee86505759de87978f8c8344245323d3bc85
juan castañeda <juanemporis123>
2022-04-21 17:51:08 -0500

    Se agrego un IMAGE VIEW a la vista y tmb se agrego un fondo de pantalla de apple

--- ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
-    @IBOutlet weak var myImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+        
     }
 
-
-}
 
