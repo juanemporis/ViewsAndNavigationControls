2dda23754bb7bac03254c21bfefa2bd3543b8b21
juan castañeda <juanemporis123>
2022-04-21 15:26:00 -0500

    Initial Commit

---
+++ ViewsAndNavigationControls/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ViewsAndNavigationControls
+//
+//  Created by wendy manrique on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
