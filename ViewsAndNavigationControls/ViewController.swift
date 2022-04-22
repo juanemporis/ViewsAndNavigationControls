19cfac5eae2a54aff22b26eeda75238979af443c
juan castañeda <juanemporis123>
2022-04-21 19:07:31 -0500

    Se agrego el STACK WIEW como ejemplo a la vista de la app

--- ViewsAndNavigationControls/ViewController.swift
+++ ViewsAndNavigationControls/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myFakeView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
         
+    @IBAction func myButtonAction(_ sender: Any) {
+        myFakeView.isHidden = true //isHidden se encarga de mostrar y ocultar unas vista
     }
+}
 
 
