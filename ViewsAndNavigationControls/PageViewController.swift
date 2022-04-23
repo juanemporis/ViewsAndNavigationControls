2b27642e9e3b39ec94650910f67821c41519442b
juan castañeda <juanemporis123>
2022-04-22 19:00:35 -0500

    Se vincularon las distintas vistas del PAGE VIEW CONTROLLER

---
+++ ViewsAndNavigationControls/PageViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PageViewController.swift
+//  ViewsAndNavigationControls
+//
+//  Created by wendy manrique on 22/04/22.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    
+    private var myControllers : [UIViewController] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let myGreenVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "vcGreen")
+        let myPinkVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "vcPink")
+        
+        myControllers.append(myGreenVC)
+        myControllers.append(myPinkVC)
+        
+        setViewControllers([myGreenVC], direction: .forward, animated: true, completion: nil)
+        
+        dataSource = self
+
+    }
+    
+}
+   
+extension PageViewController: UIPageViewControllerDataSource{
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+     
+        let index = myControllers.firstIndex(of: viewController)
+        if index == 0 {
+            return myControllers.last
+        }
+        return myControllers.first
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        let index = myControllers.firstIndex(of: viewController)
+        if index == 0 {
+            return myControllers.last
+        }
+        return myControllers.first
+    }
+    
+    
+}
+
